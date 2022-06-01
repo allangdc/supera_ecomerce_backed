5b4dbdf0dde96749c6a1ced9595207bd87ed3865
Allan Cordeiro <allangdc@gmail.com>
2022-05-31 22:59:28 -0300

    Configured postgres database

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("DBNAME"),
+        'USER': os.environ.get("DBUSER"),
+        'PASSWORD': os.environ.get("DBPASSWORD"),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
